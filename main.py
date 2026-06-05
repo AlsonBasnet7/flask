b6803ec356cc45ab8935c4c849d0bc4273cc21ab
AlsonBasnet07 <basnetalson121@gmail.com>
2026-06-05 17:25:16 +0545

    understanding the routes in the API,its the same as fastAPI.

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def display():
+    return "I am checking wheather i can run this main.py app in the local host or not"
+@app.route("/home")
 def home():
-    return "Hello, Data Scientist!"
+    return "Hey, this is the home section."
+@app.route("/contact")
+def contact():
+    return "Hey! This is the contact section of the code!"
 
 if __name__ == "__main__":
     app.run(debug=True)
