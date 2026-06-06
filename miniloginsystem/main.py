7e2efc6afee7fe0854a861c2ecbb0b4ada159eb0
AlsonBasnet07 <basnetalson121@gmail.com>
2026-06-06 19:59:23 +0545

    using flask here to write the mini login system

--- miniloginsystem/main.py
+++ miniloginsystem/main.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_templates, request
-
+from flask import Flask, render_template, request
 app =Flask(__name__)
 
-# @app.route("/")
+@app.route("/")
 # def index():
-#     return render_templates("index.html")
-@app.route("/predict", methods=["GET","POST"])
+#     return render_template("index.html")
+@app.route("/login", methods=["GET","POST"])
 def login():
     if request.method=="POST":
-     email= request.form('email')
-     password= request.form('passowrd')
-     if email =="admin@gmail.com" and password =="1234":
-        return "<h2 sytle='color:green'>Login Successful</h2>"
-     else:
-        return "<h2 sytle='color:green'>Invalid Credentials</h2>"
-     return "Thanks for using my system."
-    return render_templates("index.html")
+      email= request.form["email"]
+      password= request.form["password"]
+      print(f"The name is {email} and the password is {password}")
+      if email =="admin@gmail.com" and password =="1234":
+       return "<h2 style='color:green'>Login Successful</h2>"
+      else:
+        return "<h2 style='color:green'>Invalid Credentials</h2>"
+      return "Thanks for using my system."
+    return render_template("index.html")
 
 app.run(debug=True)
