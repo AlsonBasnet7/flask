b9af1cc348980dfb950a0965358d0b3c42d32027
AlsonBasnet07 <basnetalson121@gmail.com>
2026-06-06 17:19:05 +0545

    form handling in flask

--- day02/main.py
+++ day02/main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('form.html')
+@app.route("/")
+# def index():
+#     return render_template("form.html")
 
-@app.route('/predit', methods=['POST'])
-def predict():
-    value = request.form['feature1']
-    return f"received input:{value}"
-
+@app.route("/predict", methods=["GET","POST"])
+def hello_world():
+    if request.method=="POST":
+        name = request.form["email"]
+        password = request.form["password"]
+        print(f"The name is {name} and the password is {password}")
+        #save it to the database
+        return "<b>Thanks for using facebook. You are now logged in the system</b>"
+    return render_template("form.html")
+app.run(debug=True)
