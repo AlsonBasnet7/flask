eab855b186b8a0cb8a9047a7048e43c454400381
AlsonBasnet07 <basnetalson121@gmail.com>
2026-06-08 09:24:16 +0545

    add another route in the main.py

--- day04/main.py
+++ day04/main.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("base.html")
 app.run(debug=True)
